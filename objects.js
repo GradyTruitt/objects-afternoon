a99774143d3377bf02da9bf5a845eb58a16599fb
Grady Truitt <Grady@Gradys-MacBook-Pro.local>
2017-08-29 15:48:27 -0500

    Finished

--- objects.js
+++ objects.js
@@ -1,42 +1,79 @@
 // Create an object called user that has name, age, password properties and assign them appropriate values.
 
-// Code Here
+var user = {
+    name: "Grady Truitt",
+    age: 25,
+    password: "pa55w0rd"
+};
 
-
+console.log(user);
 
 // Add an email and username, as well as a method called alertUser that alerts the users name, to your user object using dot notation.
 
-// Code Here
+user.email = "gradytruitt@gmail.com";
+user.username = "GradyTruitt777";
+user.alertUser = function() {
+    alert(user.name);
+};
+
+console.log(user);
 
 
 
 // Now loop over your user object and alert each of the keys.
 
-// Code Here
+function alertKeys(user) {
+    for (var key in user) {
+        alert(key);
+    }
+}
+
+alertKeys(user);
 
 
 
 // Now loop over your user object and alert each of the values for each key.
 
-// Code Here
+function alertValues(user) {
+    for (var key in user) {
+        alert(user[key]);
+    }
+}
+
+alertValues(user);
 
 
 
 // Remove the username from your user object
 
-// Code Here
 
+function remove(user) {
+    for (var key in user) {
+        if (key === username) {
+            delete user[key];
+        }
+    }
+}
+
+console.log(user);
 
 
 // Create an object called creditCard that has number, cvc, type, and limit properties and assign them appropriate values.
 
-// Code Here
+var creditCard = {
+    number: 1234567869009,
+    cvc: 667,
+    type: "Visa",
+    limit: 555000
+};
+
+
 
 
 
 // Create a variable called alternating. Set it equal to the string bonusCategories.
 
-// Code Here
+var alternating = "bonusCategories";
 
 
 
@@ -51,13 +88,45 @@
     
     */
 
-// Code Here
+creditCard[alternating] = [
+    {
+        restaurants: "5%"
+    },
+    {
+        gas: "3%"
+    },
+    {
+        cabelas: "25%"
+    }
+];
+
+creditCard.bank = [{
+    name: "Centennial"
+},
+{
+    address: "1357 Falling Springs Rd. Siloam Springs, AR 72761"
+}];
+
 
 
 
 // Write a function that will take your creditCard object and user object as arguments and combine them into one object. /* HINT: You will need a for..in loop */
 
-// Code Here
+function changeCard(creditCard, user) {
+    for (var key in user) {
+        var x = 0;
+      for (var i in creditCard) {
+        if (key !== i ) {
+          x++
+        }
+      }
+      if (x > 0) {creditCard[key] = user[key];}
+    }
+}
+changeCard(creditCard, user)
+
+
+console.log(creditCard);
 
 
 /*Below you are given an object. Write a function that will loop over the allies property, remove LittleFinger, and insert Dragon Lady.
@@ -70,6 +139,22 @@ var obj = {
     alive: 'Sort of',
   };
 
-  // Code Here
+  function changeObj(obj){
+    for (var key in obj) {
+        if (key === "allies") {
+            for (var i in obj[key]) {
+                if (obj[key][i] === "LittleFinger") {
+                    obj[key][i] = "Dragon Lady"
+                }
+            }
+        }
+    }
+    for (var key2 in obj) {
+      if (key2 === "alive") {
+              obj["More Than Ever"] = obj.key2;
+              delete obj[key2];
+        }
+    }
+}
 
   
